1da968ae59b3bc9ddf66a3d9e84ca8756f6ea60a
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2018-02-22 19:30:04 +0200

    Change run_e2e default ginkgo.focus value

--- run_e2e.sh
+++ run_e2e.sh
@@ -14,7 +14,12 @@ e2e_args+=("--provider=local")
 e2e_args+=("-v")
 e2e_args+=('--test_args="${ginkgo_args[@]}"')
 e2e_args+=("--test")
-e2e_args+=(--test_args="--ginkgo.dryRun=${DRY_RUN:-false} --ginkgo.focus=${FOCUS:-\[Conformance\]}")
+if [[ -z "$FOCUS" ]]
+then
+  e2e_args+=(--test_args="--ginkgo.dryRun=${DRY_RUN:-false}")
+else
+  e2e_args+=(--test_args="--ginkgo.dryRun=${DRY_RUN:-false} --ginkgo.focus=${FOCUS}")
+fi
 
 cd $KUBE_DIR
 go run $E2E_RUN "${e2e_args[@]}" | tee ../$RESULTS_DIR/acs_run_$(date +%Y_%m_%d_%H_%M)

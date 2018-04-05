db5a32629023895ce0ccefac8b27212d930e736a
Claudiu Belu <cbelu@cloudbasesolutions.com>
2018-04-04 17:00:32 -0700

    Adds skip tests option to run_e2e.sh
    Adds the 3rd argument to run_e2e.sh, allowing it to accept
    a file containing tests to be skipped.

--- run_e2e.sh
+++ run_e2e.sh
@@ -6,7 +6,7 @@ if [ -z "$KUBE_TEST_REPO_LIST" ]; then echo "Need to set KUBE_TEST_REPO_LIST"; e
 
 if [ $# -lt 2 ]
 then
-  echo "Usage: $0 <is_dry_run> <focus_tests_file>"
+  echo "Usage: $0 <is_dry_run> <focus_tests_file> [skip_tests_file]"
   exit 1
 fi
 
@@ -19,24 +19,35 @@ set -o errexit
 
 DRY_RUN=$1
 FOCUS=$2
+SKIP=$3
 
-while IFS= read -r line
-do
-  # skip comments in the file.
-  if [ "$line" = "" ] || [ "#" = `echo $line | cut -b 1` ]
-  then
-    continue
-  fi
+function get_tests_regex() {
+  local tests_file=$1
 
-  if [ "$TEXT" = "" ]
-  then
-    TEXT="$line"
-  else
-    TEXT="${line}|${TEXT}"
-  fi
-done < "$FOCUS"
+  while IFS= read -r line
+  do
+    # skip comments in the file.
+    if [ "$line" = "" ] || [ "#" = `echo $line | cut -b 1` ]
+    then
+      continue
+    fi
 
-FOCUS="$TEXT"
+    if [ "$TEXT" = "" ]
+    then
+      TEXT="$line"
+    else
+      TEXT="${line}|${TEXT}"
+    fi
+  done < "$tests_file"
+
+  echo $TEXT
+}
+
+FOCUS="`get_tests_regex $FOCUS`"
+if [[ -n "$SKIP" ]]
+then
+  SKIP="`get_tests_regex $SKIP`"
+fi
 
 BASE_DIR=$(dirname "${BASH_SOURCE}")
 KUBE_DIR=$BASE_DIR/kubernetes
@@ -44,17 +55,22 @@ E2E_RUN=hack/e2e.go
 RESULTS_DIR=$BASE_DIR/results
 mkdir -p $RESULTS_DIR
 
+ginkgo_args="--ginkgo.dryRun=${DRY_RUN:-false} "
+
+if [[ -n "$FOCUS" ]]
+then
+  ginkgo_args+="--ginkgo.focus=${FOCUS} "
+fi
+if [[ -n "$SKIP" ]]
+then
+  ginkgo_args+="--ginkgo.skip=${SKIP} "
+fi
+
 e2e_args=("--")
 e2e_args+=("--provider=local")
 e2e_args+=("-v")
-e2e_args+=('--test_args="${ginkgo_args[@]}"')
 e2e_args+=("--test")
-if [[ -z "$FOCUS" ]]
-then
-  e2e_args+=(--test_args="--ginkgo.dryRun=${DRY_RUN:-false}")
-else
-  e2e_args+=(--test_args="--ginkgo.dryRun=${DRY_RUN:-false} --ginkgo.focus=${FOCUS}")
-fi
+e2e_args+=(--test_args="$ginkgo_args")
 
 cd $KUBE_DIR
 go run $E2E_RUN "${e2e_args[@]}" | tee ../$RESULTS_DIR/acs_run_$(date +%Y_%m_%d_%H_%M)

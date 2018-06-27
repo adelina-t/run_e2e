d4e89addb49bfb5086edbdcf3ccef4497f4b0a56
TaEduard <etamsa@cloudbasesolutions.com>
2018-06-27 19:16:54 +0300

    fixes to run out of the box
        - env: ubuntu 18.04 server

--- build_kubernetes.sh
+++ build_kubernetes.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 set -o errexit
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/kubernetes
 
 echo "Building Kubernetes"
 
-$KUBE_ROOT/build/run.sh make all
+sudo $KUBE_ROOT/build/run.sh make WHAT="test/e2e/e2e.test cmd/kubectl vendor/github.com/onsi/ginkgo/ginkgo"
--- install_prereq.sh
+++ install_prereq.sh
@@ -1,36 +1,57 @@
-#update
-sudo apt-get update -y
+#!/bin/bash
+# This script is used to install docker golang and build kubernetes
 
-#install docker and golang, build kubernetes
-
-set -o errexit
-
-GO_VERSION="1.10"
+set -e -o pipefail
 
 BASEDIR=$(dirname "${BASH_SOURCE}")
 WIN_E2E_KUBE_BRANCH="win_e2e_testing"
 KUBE_REPO="http://github.com/adelina-t/kubernetes"
+GO_VERSION="1.10"
 
-echo "Installing docker"
-sudo apt-get install -y docker.io
+sudo apt-get update -y
 
-echo "Adding user $USER to docker group"
-sudo usermod -a -G docker $USER
+function program_is_installed() {
+  local return_=1
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  echo "$return_"
+}
 
+function install_go() {
+  echo "Installing golang"
 
-echo "Installing golang"
-sudo add-apt-repository ppa:gophers/archive
-sudo apt-get update
-sudo apt-get install golang-$GO_VERSION-go
+  DEBIAN_FRONTEND=noninteractive sudo apt-get install golang-$GO_VERSION-go -y
+  mkdir -p $HOME/go/{bin,pkg,src}
 
-export GOROOT="/usr/lib/go-$GO_VERSION"
-export PATH=$GOROOT/bin:$PATH
+  echo "Make sure you properly set the following env variables"
+  echo "GO_VERSION=$GO_VERSION"
+  echo 'export GOROOT=/usr/lib/go-$GO_VERSION'
+  echo 'export GOBIN=$GOROOT/bin'
+  echo 'export GOPATH="$HOME/go"'
+  echo 'export PATH=$GOROOT/bin:$PATH:$GOPATH/bin'
+}
 
-echo "Cloning Kubernetes repo $KUBE_REPO"
+function install_docker(){
+  if [[ $(program_is_installed docker) != "1" ]]; then
+    echo "Installing docker"
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install docker.io -y
 
-git clone $KUBE_REPO
-cd $BASEDIR/kubernetes
-git checkout origin/$WIN_E2E_KUBE_BRANCH -b win_e2e_testing
+    echo "Adding user $USER to docker group"
+    sudo usermod -a -G docker $USER
+  fi
+}
 
+function clone_k8s() {
+  if [ ! -d "$BASEDIR/kubernetes" ]; then
+    echo "Cloning Kubernetes repo $KUBE_REPO"
+    git clone $KUBE_REPO
+    pushd $BASEDIR/kubernetes
+    	git checkout origin/$WIN_E2E_KUBE_BRANCH -b $WIN_E2E_KUBE_BRANCH
+    popd
+  fi
+}
 
+install_go
+install_docker
+clone_k8s
 
+newgrp docker
